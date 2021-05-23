02c529022ef093d6089abb11f9ce43add98f91fb
Seb735 <duclutsebastien@yahoo.com>
2021-05-23 15:39:24 +0200

    Mise à plat des branches

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        if ($this->getUser()){
+            return $this->redirectToRoute('dashboard');
+        }
+
+        return $this->render('home/index.html.twig');
+    }
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboard(): Response
+    {
+        return $this->render('home/dashboard.html.twig');
+    }
+}
---
+++ src/Form/Food1Type.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Food;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Food1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('quantity')
+            ->add('expirationDate')
+            ->add('isOutOfDate')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('familyfood')
+            ->add('preservation')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Food::class,
+        ]);
+    }
+}
